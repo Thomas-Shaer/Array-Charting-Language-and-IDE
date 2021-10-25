9c7d18fa7ac6e0b1ba06c1064816297947d11cfb
Tom Shaer <txs941@cs.bham.ac.uk>
2021-10-25 17:16:15 +0100

    Basic AST Nodes

--- CodeBase/CodeBase/main.cpp
+++ CodeBase/CodeBase/main.cpp
@@ -1,10 +1,13 @@
 
 #include <iostream>
-
+#include "node.h"
 
 int main() {
 
-	std::cout << "hello" << std::endl;
+	AssignNode* assignNode = new AssignNode(new IdentifierNode("x"), new BinaryOpNode(new NumberNode(2), BinaryOperator::PLUS, new NumberNode(132)));
+
+
+	std::cout << assignNode->toString() << std::endl;
 
 	return 0;
 }
---
+++ CodeBase/CodeBase/node.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <iostream>
+#include <string>
+const enum class BinaryOperator {PLUS, MINUS, MUL, DIVIDE};
+
+/*
+Base node class
+*/
+class Node {
+public:
+
+
+	virtual std::string toString() const = 0;
+};
+
+/*
+Number node class, represents a number node e.g. 2
+*/
+class NumberNode : public Node {
+public:
+	const int number;
+	NumberNode(int _number) : number(_number) {}
+
+	virtual std::string toString() const {
+		return std::to_string(number);
+	}
+};
+
+/*
+Identifier node class, represents identifier e.g. foo
+*/
+class IdentifierNode : public Node {
+public:
+	const std::string name;
+	IdentifierNode(std::string _name) : name(_name) {}
+
+	virtual std::string toString() const {
+		return name;
+	}
+};
+
+/*
+Binary operator node, used for binary operations e.g. 2 + 3
+*/
+class BinaryOpNode : public Node {
+public:
+	const Node* left;
+	const Node* right;
+	const BinaryOperator op;
+	BinaryOpNode(Node* _left, BinaryOperator _op, Node* _right) : left(_left), right(_right), op(_op) {}
+
+	virtual std::string toString() const {
+		return left->toString() + " + " + right->toString();
+	}
+};
+
+
+/*
+Assign node, used for assign statements, e.g. x = 2 + 3
+*/
+class AssignNode : public Node {
+public:
+	const IdentifierNode* lhs;
+	const Node* rhs;
+	AssignNode(IdentifierNode* _lhs, Node* _rhs) : lhs(_lhs), rhs(_rhs) {}
+
+	virtual std::string toString() const {
+		return lhs->toString() + " = " + rhs->toString();
+	}
+};
